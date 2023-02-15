b8ce90ddd2923e3216c932e74f5063b4ef690ffb
luismartorano <luis.tavares@vitta.me>
2023-02-15 15:00:11 -0300

    terraform code

--- main.tf
+++ main.tf
@@ -2,6 +2,29 @@ provider "aws" {
   region = var.aws_region
 }
 
+terraform {
+  required_version = ">= 0.15"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.34.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.9.1"
+    }
+  }
+}
+
+module "secretsmanager-to-vault-migration" {
+  source  = "samgabrail/secretsmanager-to-vault-migration/vault"
+  version = "0.0.4" # use the latest version or pin a version
+  # insert at least the 1 required variable here
+  secret_names = ["samg-migration-vault", "samg-migration-vault2"]
+}
+
+
+
 data "aws_secretsmanager_secret" "mysecret" {
   for_each = toset(var.secret_names)
   name     = each.value
